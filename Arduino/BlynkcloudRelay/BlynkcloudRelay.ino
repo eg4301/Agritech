aad5d894288513436d4f622df6aceaf6c144d83f
Usenmer1 <117057748+Usenmer1@users.noreply.github.com>
2023-09-27 01:01:08 +0900

    added water sensor

--- Arduino/BlynkcloudRelay/BlynkcloudRelay.ino
+++ Arduino/BlynkcloudRelay/BlynkcloudRelay.ino
@@ -29,7 +29,13 @@
 #define RELAY_PIN_2 19
 #define RELAY_PIN_3 18
 #define RELAY_PIN_4 5
+
 #define LED_PIN     25
+
+#define WATER_POWER_PIN  27
+#define WATER_SENS_PIN 23
+
+int water_value = 0; // variable to store the water sensor value
 
 int PUMP_DURATION;
 
@@ -58,79 +64,95 @@ char pass[] = "owaibohvae";
 
 BLYNK_WRITE(V0)
 {
-    if (param.asInt() == 1) {
-        digitalWrite(RELAY_PIN_1, HIGH);
-        Blynk.logEvent("relay_state", "RELAY_1 ON");//Sending Events
-    } else {
-        digitalWrite(RELAY_PIN_1,  LOW);
-        Blynk.logEvent("relay_state", "RELAY_1 OFF");//Sending Events
-    }
+  if (param.asInt() == 1) {
+    digitalWrite(RELAY_PIN_1, HIGH);
+    Blynk.logEvent("relay_state", "RELAY_1 ON");//Sending Events
+  } else {
+    digitalWrite(RELAY_PIN_1,  LOW);
+    Blynk.logEvent("relay_state", "RELAY_1 OFF");//Sending Events
+  }
 }
 
 BLYNK_WRITE(V1)
 {
-    if (param.asInt() == 1) {
-        digitalWrite(RELAY_PIN_2, HIGH);
-        Blynk.logEvent("relay_state", "RELAY_2 ON");//Sending Events
-    } else {
-        digitalWrite(RELAY_PIN_2,  LOW);
-        Blynk.logEvent("relay_state", "RELAY_2 OFF");//Sending Events
-    }
+  if (param.asInt() == 1) {
+    digitalWrite(RELAY_PIN_2, HIGH);
+    Blynk.logEvent("relay_state", "RELAY_2 ON");//Sending Events
+  } else {
+    digitalWrite(RELAY_PIN_2,  LOW);
+    Blynk.logEvent("relay_state", "RELAY_2 OFF");//Sending Events
+  }
 }
 
 BLYNK_WRITE(V2)
 {
-    if (param.asInt() == 1) {
-        digitalWrite(RELAY_PIN_3, HIGH);
-        Blynk.logEvent("relay_state", "RELAY_3 ON");//Sending Events
-    } else {
-        digitalWrite(RELAY_PIN_3, LOW);
-        Blynk.logEvent("relay_state", "RELAY_3 OFF");//Sending Events
-    }
+  if (param.asInt() == 1) {
+    digitalWrite(RELAY_PIN_3, HIGH);
+    Blynk.logEvent("relay_state", "RELAY_3 ON");//Sending Events
+  } else {
+    digitalWrite(RELAY_PIN_3, LOW);
+    Blynk.logEvent("relay_state", "RELAY_3 OFF");//Sending Events
+  }
 }
 
 BLYNK_WRITE(V3)
 {
-    if (param.asInt() == 1) {
-        digitalWrite(RELAY_PIN_4, HIGH );
-        Blynk.logEvent("relay_state", "RELAY_4 ON");//Sending Events
-        delay(PUMP_DURATION);
-        digitalWrite(RELAY_PIN_4, LOW );
-        Blynk.logEvent("relay_state", "RELAY_4 OFF");//Sending Events
-    } else {
-        digitalWrite(RELAY_PIN_4, LOW);
-        Blynk.logEvent("relay_state", "RELAY_4 OFF");//Sending Events
-    }
+  if (param.asInt() == 1) {
+    digitalWrite(RELAY_PIN_4, HIGH );
+    Blynk.logEvent("relay_state", "RELAY_4 ON");//Sending Events
+    delay(PUMP_DURATION);
+    digitalWrite(RELAY_PIN_4, LOW );
+    Blynk.logEvent("relay_state", "RELAY_4 OFF");//Sending Events
+  } else {
+    digitalWrite(RELAY_PIN_4, LOW);
+    Blynk.logEvent("relay_state", "RELAY_4 OFF");//Sending Events
+  }
 }
 
 BLYNK_WRITE(V4)
 {
-    PUMP_DURATION = param.asInt();
+  PUMP_DURATION = param.asInt();
+}
+
+BLYNK_WRITE(V5)
+{
+  digitalWrite(WATER_POWER_PIN, HIGH);  // turn the sensor ON
+  delay(10);                      // wait 10 milliseconds
+  water_value = analogRead(WATER_SENS_PIN); // read the analog value from sensor
+  digitalWrite(WATER_POWER_PIN, LOW);   // turn the sensor OFF
+  Blynk.virtualWrite(V5, water_value);
+
+  delay(1000);
 }
 
 //Syncing the output state with the app at startup
 BLYNK_CONNECTED()
 {
-    Blynk.syncVirtual(V4);  // will cause BLYNK_WRITE(V4) to be executed
-    Blynk.syncVirtual(V0);  // will cause BLYNK_WRITE(V0) to be executed
-    Blynk.syncVirtual(V1);  // will cause BLYNK_WRITE(V1) to be executed
-    Blynk.syncVirtual(V2);  // will cause BLYNK_WRITE(V2) to be executed
-    Blynk.syncVirtual(V3);  // will cause BLYNK_WRITE(V3) to be executed
+  Blynk.syncVirtual(V4);  // will cause BLYNK_WRITE(V4) to be executed
+  Blynk.syncVirtual(V5);  // will cause BLYNK_WRITE(V5) to be executed
+  Blynk.syncVirtual(V0);  // will cause BLYNK_WRITE(V0) to be executed
+  Blynk.syncVirtual(V1);  // will cause BLYNK_WRITE(V1) to be executed
+  Blynk.syncVirtual(V2);  // will cause BLYNK_WRITE(V2) to be executed
+  Blynk.syncVirtual(V3);  // will cause BLYNK_WRITE(V3) to be executed
 }
 
 void setup()
 {
-    // Debug console
-    Serial.begin(115200);
+  // Debug console
+  Serial.begin(115200);
+  pinMode(LED_PIN, OUTPUT);
 
-    pinMode(RELAY_PIN_1, OUTPUT);
-    pinMode(RELAY_PIN_2, OUTPUT);
-    pinMode(RELAY_PIN_3, OUTPUT);
-    pinMode(RELAY_PIN_4, OUTPUT);
-    pinMode(LED_PIN, OUTPUT);
-    delay(100);
+  pinMode(RELAY_PIN_1, OUTPUT);
+  pinMode(RELAY_PIN_2, OUTPUT);
+  pinMode(RELAY_PIN_3, OUTPUT);
+  pinMode(RELAY_PIN_4, OUTPUT);
+  
+  pinMode(WATER_POWER_PIN, OUTPUT);   // configure D7 pin as an OUTPUT
+  digitalWrite(WATER_POWER_PIN, LOW); // turn the sensor OFF
 
-    Blynk.begin(auth, ssid, pass);
+  delay(100);
+
+  Blynk.begin(auth, ssid, pass);
 }
 
 void loop()
