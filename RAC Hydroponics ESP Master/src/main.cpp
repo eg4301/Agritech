00c8bcfb5b28da85806e6a7d150240eb7196734a
Ian-Snoddy <86479941+Ian-Snoddy@users.noreply.github.com>
2024-02-10 19:55:09 +0800

    Update main.cpp
    Added AWS Reconnection and OTA

--- RAC Hydroponics ESP Master/src/main.cpp
+++ RAC Hydroponics ESP Master/src/main.cpp
@@ -19,6 +19,11 @@
 #include <ESP32Ping.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
+
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+#include <Update.h>
  
  
 #define AWS_IOT_PUBLISH_TOPIC   "esp32/pub"
@@ -38,6 +43,7 @@ uint16_t lastreceived;
 // std::queue<float> conduct = {};  // queue for conductivity values
 // std::queue<float> pH = {};       // queue for pH values
 
+// Variables for logging data
 float temp_now = 0;
 float con_now = 0;
 float pH_now = 0;
@@ -47,8 +53,11 @@ float CO2_now = 0;
 float oxy_now = 0;
 int MAC_now = 0;
 
+/*Variables for HEX Concetenation*/
 uint16_t HEX_A;
 uint16_t HEX_B;
+
+/*Boolean to check if need to send data*/ 
 bool is_send_data = false;
 
 String formattedDate;
@@ -67,7 +76,12 @@ float timezone;
 ESP32Time rtc;
 uint64_t seconds, minutes, hours, days, months, year;
 
+// OTA
+const char* host = "esp32";
 
+// WiFi Reconnect
+unsigned long previousMillis = 0;
+unsigned long interval = 30000;
 
 
 /**
@@ -219,6 +233,93 @@ void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
   
   
 }
+
+WebServer server(80);
+
+/*
+ * Login page
+ */
+const char* loginIndex = 
+ "<form name='loginForm'>"
+    "<table width='20%' bgcolor='A09F9F' align='center'>"
+        "<tr>"
+            "<td colspan=2>"
+                "<center><font size=4><b>ESP32 Login Page</b></font></center>"
+                "<br>"
+            "</td>"
+            "<br>"
+            "<br>"
+        "</tr>"
+        "<td>Username:</td>"
+        "<td><input type='text' size=25 name='userid'><br></td>"
+        "</tr>"
+        "<br>"
+        "<br>"
+        "<tr>"
+            "<td>Password:</td>"
+            "<td><input type='Password' size=25 name='pwd'><br></td>"
+            "<br>"
+            "<br>"
+        "</tr>"
+        "<tr>"
+            "<td><input type='submit' onclick='check(this.form)' value='Login'></td>"
+        "</tr>"
+    "</table>"
+"</form>"
+"<script>"
+    "function check(form)"
+    "{"
+    "if(form.userid.value=='EG4301' && form.pwd.value=='agritech.1')"
+    "{"
+    "window.open('/serverIndex')"
+    "}"
+    "else"
+    "{"
+    " alert('Error Password or Username')/*displays error message*/"
+    "}"
+    "}"
+"</script>";
+ 
+/*
+ * Server Index Page
+ */
+ 
+const char* serverIndex = 
+"<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
+"<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
+   "<input type='file' name='update'>"
+        "<input type='submit' value='Update'>"
+    "</form>"
+ "<div id='prg'>progress: 0%</div>"
+ "<script>"
+  "$('form').submit(function(e){"
+  "e.preventDefault();"
+  "var form = $('#upload_form')[0];"
+  "var data = new FormData(form);"
+  " $.ajax({"
+  "url: '/update',"
+  "type: 'POST',"
+  "data: data,"
+  "contentType: false,"
+  "processData:false,"
+  "xhr: function() {"
+  "var xhr = new window.XMLHttpRequest();"
+  "xhr.upload.addEventListener('progress', function(evt) {"
+  "if (evt.lengthComputable) {"
+  "var per = evt.loaded / evt.total;"
+  "$('#prg').html('progress: ' + Math.round(per*100) + '%');"
+  "}"
+  "}, false);"
+  "return xhr;"
+  "},"
+  "success:function(d, s) {"
+  "console.log('success!')" 
+ "},"
+ "error: function (a, b, c) {"
+ "}"
+ "});"
+ "});"
+ "</script>";
  
 void setup() {
   Serial.begin(115200);  
@@ -238,6 +339,51 @@ void setup() {
   WiFi.enableLongRange(true);
   // WiFi.setTxPower(WIFI_POWER_19_5dBm);
 
+  // Start Web Server for OTA firmware flashing
+  /*use mdns for host name resolution*/
+  if (!MDNS.begin(host)) { //http://esp32.local
+    Serial.println("Error setting up MDNS responder!");
+    while (1) {
+      delay(1000);
+    }
+  }
+  Serial.println("mDNS responder started");
+  /*return index page which is stored in serverIndex */
+  server.on("/", HTTP_GET, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", loginIndex);
+  });
+  server.on("/serverIndex", HTTP_GET, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", serverIndex);
+  });
+  /*handling uploading firmware file */
+  server.on("/update", HTTP_POST, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+    ESP.restart();
+  }, []() {
+    HTTPUpload& upload = server.upload();
+    if (upload.status == UPLOAD_FILE_START) {
+      Serial.printf("Update: %s\n", upload.filename.c_str());
+      if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_WRITE) {
+      /* flashing firmware to ESP*/
+      if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_END) {
+      if (Update.end(true)) { //true to set the size to the current progress
+        Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+      } else {
+        Update.printError(Serial);
+      }
+    }
+  });
+  server.begin();
+
   // Init ESP-NOW
   if (esp_now_init() != ESP_OK)
   {
@@ -249,6 +395,15 @@ void setup() {
 }
  
 void loop(){
+  unsigned long currentMillis = millis();
+  // if WiFi is down, try reconnecting
+  if ((WiFi.status() != WL_CONNECTED) && (currentMillis - previousMillis >=interval)) {
+    Serial.print(millis());
+    Serial.println("Reconnecting to WiFi...");
+    WiFi.disconnect();
+    connectAWS();
+    previousMillis = currentMillis;
+  }
  
   if (is_send_data)
     {
@@ -262,5 +417,6 @@ void loop(){
   // delay(10000);
 
   client.loop();
+  server.handleClient();
   delay(1000);
 }
